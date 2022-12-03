e5292730c7c0735b21e12e6ed6b281926d78c1ca
codeplayon <codeplayon@gmail.com>
2022-12-03 14:42:04 +0530

    Initial commit

---
+++ app/src/main/java/com/codeplayon/scratchcardexample/MainActivity.java
@@ -0,0 +1,40 @@
+package com.codeplayon.scratchcardexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.codeplayon.scratchview.ScratchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScratchView scratchView;
+    Button mask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mask = findViewById(R.id.mask);
+        scratchView = findViewById(R.id.scratchView);
+
+        scratchView.setRevealListener(new ScratchView.IRevealListener() {
+            @Override
+            public void onRevealed(ScratchView scratchView) {
+                Toast.makeText(getApplicationContext(), "Reveled", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onRevealPercentChangedListener(ScratchView scratchView, float percent) {
+                if (percent >= 0.7) {
+                    Log.d("Reveal Percentage", "onRevealPercentChangedListener: " + percent);
+                }
+            }
+        });
+
+        mask.setOnClickListener(v -> scratchView.mask());
+    }
+}
